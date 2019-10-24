f626dc115a5af362c951487f7a294734d35f68ef
BertoLightYear <49400072+BertoLightYear@users.noreply.github.com>
2019-10-24 14:50:31 +0200

    Primera Subida

---
+++ 1.Presentacion/Presentacion/BL/Interfaces/IMonedaManager.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Collections.Generic; 
+
+namespace Presentacion.BL.Interfaces
+{
+    interface IMonedaManager
+    {
+        List<Models.Moneda> GetMonedas();
+    }
+}
---
+++ 1.Presentacion/Presentacion/Models/Moneda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Presentacion.Models
+{
+    public class Moneda
+    {
+        public Guid Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public decimal Valor { get; set; }
+
+    }
+}
